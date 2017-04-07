3ef2f48524a2b2e84d9fcbf856a2ccafa280cc04
Alex <alexandersuglio@gmail.com>
2017-04-07 18:06:11 -0400

    Test commit on Alex's end

--- client/main.js
+++ client/main.js
@@ -1,5 +1,22 @@
-$(".button-collapse").sideNav();
+// $(".button-collapse").sideNav();
 
 FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
 });
+
+// function hey(){
+//     alert('hey');
+// };
+//
+// hey();
+
+
+// $("#contact").hide();
+// $("#about").hide();
+//
+// $("#meetTeamClick").on("click", function(){
+//     $("#about").show();
+// });
+
+// $("")
+
